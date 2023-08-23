02355b9def9106268aec8e6e0ec78977a71d9907
Tronikelis <contactdonatas@gmail.com>
2023-08-23 19:05:22 +0300

    adding react frontend

---
+++ frontend/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const element = document.getElementById("react-root") as HTMLDivElement;
+const root = createRoot(element);
+root.render(<App />);
+
+function App() {
+    return <div>Hello mens</div>;
+}
